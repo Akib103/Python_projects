450add59a7cc155462be534b1a9b287abbdd636b
Akib103 <akibalmuyeed@gmail.com>
2024-06-05 04:22:25 -0500

    Add files

---
+++ LightOJ/Opposite Task.py
@@ -0,0 +1,12 @@
+n=int(input())
+for i in range(1,n+1):
+    a=int(input())
+    if a>11:
+        print((a-10), 10)
+    elif a==0:
+        print(0,0)
+    else:
+        print((a-1), 1)
+    
+          
+    
