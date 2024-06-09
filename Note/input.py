ce67d96cf8307c61160025cc02856858464c368f
Akib103 <akibalmuyeed@gmail.com>
2024-06-09 14:30:43 -0500

    Add file

---
+++ Note/input.py
@@ -0,0 +1,4 @@
+a=int(input())
+b=input() # b would be string by default
+c,d=map(int,input().split())
+e=list(map(int,input().split())) # all the values will be stored in a list and they are separated by a space
