ddef5e3e17c90b0ea4f55776c5aa57e4b29f109e
Akib103 <akibalmuyeed@gmail.com>
2024-06-12 12:44:08 -0500

    Update file

--- Codeforces/nearly lucky number.py
+++ Codeforces/nearly lucky number.py
@@ -4,9 +4,7 @@ for i in range(0,len(n)):
     count=count
     if n[i]==str(4) or n[i]==str(7):
         count+=1
-if int(n)==4 or int(n)==7:
-    print("YES")
-elif count==4 or count==7:
+if count==4 or count==7:
     print("YES")
 else:
     print("NO")
