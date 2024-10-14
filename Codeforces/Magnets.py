88402af456b8d4fe0dcc1d8853467a500d655aa7
Akib Al Muyeed <akibalmuyeed@gmail.com>
2024-10-14 20:51:37 +0600

    Add files

---
+++ Codeforces/Magnets.py
@@ -0,0 +1,5 @@
+n=int(input())
+orientation=[]
+for i in range (int(n)):
+    a=(int(input({i})))
+    if a 
---
+++ Codeforces/watermelon.py
@@ -0,0 +1,9 @@
+weight=int(input())
+if weight == 2:
+    print("No") 
+elif weight == 0:
+    print("No")
+elif weight%2==0:
+    print("Yes")
+else:
+    print("No")
